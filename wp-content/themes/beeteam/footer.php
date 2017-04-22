5e705a59795f8f249e9c4b4d9b49d5aee9d2802c
John Jezon <johnjezonajias@gmail.com>
2017-04-22 18:50:55 +0800

    no message

--- wp-content/themes/beeteam/footer.php
+++ wp-content/themes/beeteam/footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'beeteam' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'beeteam' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'beeteam' ), 'beeteam', '<a href="https://automattic.com/" rel="designer">BeeThemes</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'beeteam' ), 'beeteam', '<a href="https://beethemes.com/" rel="designer">BeeThemes</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
